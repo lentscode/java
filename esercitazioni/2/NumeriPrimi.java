393f6c7bd0d0618cfb0d854678bf7215b8dc1d51
Lentscode <antonio.lentini2004@gmail.com>
2025-03-10 09:22:30 +0100

    🚀 Complete NumeriPrimi

--- esercitazioni/2/NumeriPrimi.java
+++ esercitazioni/2/NumeriPrimi.java
@@ -1,27 +1,22 @@
-import java.util.*;
 
 class NumeriPrimi {
 	public static void main(String[] args) {
-		List<int> numeriPrimi = new ArrayList();
+		int[] numeriPrimi = new int[50];
+		int currentIndex = 0;
 
-		int current = 2;
-
-		while (current < 100) {
-			boolean primo = true;
-			for (int i = 0; i < numeriPrimi.size(); i++) {
-				if (current % numeriPrimi[i] == 0) {
-					primo = false;
+		for (int i = 2; i < 100; i++) {
+			boolean prime = true;
+			for (int j = 0; j < currentIndex; j++) {
+				if (i % numeriPrimi[j] == 0 ) {
+					prime = false;
 					break;
 				}
 			}
 
-			if (primo) {
-				numeriPrimi.add(current);
+			if (prime) {
+				numeriPrimi[currentIndex++] = i;
+				System.out.println(String.format("Numero primo Nº%d: %d", currentIndex, i));
 			}
-
-			current++;
 		}
-
-		System.out.println(numeriPrimi);
 	}
 }
